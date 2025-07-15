2d272bb96ceae84e98b43dcb9f6f89389ac745c6
skhasan536 <hasan.shaikh@wybrid.com>
2025-07-15 11:09:26 +0530

    added config module to ensure security

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,17 +1,17 @@
-//database connection
+// connectDB.js
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import config from "config";
 
-dotenv.config();
+const mongodbUrl = config.get("mongodb.url");
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected!")
-    }
-    catch(error){
-        console.error("MongoDB connection failed", error);
+    try {
+        await mongoose.connect(mongodbUrl);
+        console.log("MongoDB connected!");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
         process.exit(1);
     }
 };
+
 export default connectDB;
