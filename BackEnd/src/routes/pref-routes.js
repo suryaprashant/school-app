df6f7759b119a6fb99b2bd441af2065bc6d21242
uvica <uvicaahts@gmail.com>
2025-09-24 17:51:42 +0545

    added new route file for pref

---
+++ BackEnd/src/routes/pref-routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { 
+  addPreference, 
+  updatePreference, 
+  getPreference, 
+  predictSchoolPerformance 
+} from "../controllers/pref-controller.js";
+
+const router = express.Router();
+
+// Add new preference
+router.post("/", addPreference);
+
+// Update preference
+router.put("/:studId", updatePreference);
+
+// Get preference by student ID
+router.get("/:studId", getPreference);
+
+// Predict schools based on student preference
+router.get("/predict/:studentId", predictSchoolPerformance);
+
+export default router;
